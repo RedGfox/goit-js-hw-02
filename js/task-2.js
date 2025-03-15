b56b4b722f1838319f6604de33de7fc43cdef60b
RG <zim238kosmit@gmail.com>
2025-03-15 23:08:59 +0200

    hwdane

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,8 @@
 function formatMessage(message, maxLength) {
   if (message.length <= maxLength) {
-    return message.slice();
-  } else if (message.length > maxLength) {
-    return message.slice(0, maxLength) + '...';
+    return message;
   }
+  return message.slice(0, maxLength) + '...';
 }
 
 console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
